17a3be09e0e2c26b982829766565b8bfc56d88b1
Joe Christensen <joechristensenwork@gmail.com>
2021-08-16 12:04:54 -0700

    commit tree a9cac9fa194a8178c27519ab74c7621395362fc0

---
+++ lib/insert.sql
@@ -0,0 +1,41 @@
+INSERT INTO series VALUES (1, "title", 1, 1);
+INSERT INTO series VALUES (2, "title", 1, 1);
+
+INSERT INTO subgenres VALUES (1, "title");
+INSERT INTO subgenres VALUES (2, "title");
+
+INSERT INTO authors VALUES (1, "name");
+INSERT INTO authors VALUES (2, "name");
+
+INSERT INTO books VALUES (1, "title", 2000, 1);
+INSERT INTO books VALUES (2, "title", 2000, 1);
+INSERT INTO books VALUES (3, "title", 2000, 1);
+INSERT INTO books VALUES (4, "title", 2000, 2);
+INSERT INTO books VALUES (5, "title", 2000, 2);
+INSERT INTO books VALUES (6, "title", 2000, 2);
+
+INSERT INTO characters VALUES (1, "name", "motto", "human", 1);
+INSERT INTO characters VALUES (2, "name", "motto", "human", 1);
+INSERT INTO characters VALUES (3, "name", "motto", "human", 1);
+INSERT INTO characters VALUES (4, "name", "motto", "human", 1);
+INSERT INTO characters VALUES (5, "name", "motto", "human", 2);
+INSERT INTO characters VALUES (6, "name", "motto", "human", 2);
+INSERT INTO characters VALUES (7, "name", "motto", "human", 2);
+INSERT INTO characters VALUES (8, "name", "motto", "human", 2);
+
+INSERT INTO character_books VALUES (1, 1, 1);
+INSERT INTO character_books VALUES (2, 2, 1);
+INSERT INTO character_books VALUES (3, 3, 1);
+INSERT INTO character_books VALUES (4, 4, 1);
+INSERT INTO character_books VALUES (5, 1, 1);
+INSERT INTO character_books VALUES (6, 2, 1);
+INSERT INTO character_books VALUES (7, 3, 1);
+INSERT INTO character_books VALUES (8, 4, 1);
+INSERT INTO character_books VALUES (9, 1, 1);
+INSERT INTO character_books VALUES (10, 2, 1);
+INSERT INTO character_books VALUES (11, 3, 1);
+INSERT INTO character_books VALUES (12, 4, 1);
+INSERT INTO character_books VALUES (13, 1, 1);
+INSERT INTO character_books VALUES (14, 2, 1);
+INSERT INTO character_books VALUES (15, 3, 1);
+INSERT INTO character_books VALUES (16, 4, 1);
